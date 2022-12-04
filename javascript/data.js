0d98e238cd77a20474cdbb1e3f945d77ad6e0aad
HanhDinh1 <ms.myhanh89@gmail.com>
2022-12-04 00:28:50 -0500

    starter files

---
+++ javascript/data.js
@@ -0,0 +1,42 @@
+const comSuon = [
+    "season pork generously with salt, pepper and garlic powder",
+    "place in a bowl",
+    "cover the bowl and let sit for 15 minutes",
+    "wash 3 cups of jasmine rice"
+    "cook rice for 30 minutes",
+    "grill the pork medium heat until golden both sides using a brush to baste with oil",
+    "cut green onion",
+    "heat pan with peanut oil and fry the green onion",
+    "put rice, grilled pork and green onion together",
+    "enjoy",
+];
+
+const mashedPotatoes = [
+    "boil water",
+    "tear open bag of of instant potato mix and pour into bowl",
+    "pour in water",
+    "mix",
+    "enjoy",
+  ];
+
+  const brusselsSprouts = [
+    'wash brussels sprouts',
+    'cut off base and chop in half',
+    'toss in bowl with olive oil, balsamic vinegar and salt',
+    'preheat oven to 500 F',
+    'coat baking sheet with olive oil',
+    'roast in the oven for 20 minutes',
+    'place back in bowl and add salt and pepper',
+    'enjoy'
+  ];
+
+  const steak = [
+    "season steak generously with salt, pepper and garlic powder",
+    "place in zip lock bag",
+    "cook in sous vide at 120 F for 1-2 hours",
+    "remove from bag and pat dry",
+    "heat pan with grapeseed oil and a quarter stick of butter",
+    "cook steak for 30-60 seconds per side using a spoon to baste with butter",
+    "rest for 10 minutes",
+    "enjoy",
+  ];
